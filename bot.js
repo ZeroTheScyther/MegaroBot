361f91253c5d3eb1f363f01bf05a1fc1d674e731
Glitch (megarobot) <none>
2020-01-14 10:41:35 +0000

    👜🌎 Updated with Glitch

--- bot.js
+++ bot.js
@@ -1,7 +1,21 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require('./config.json');
 const fs = require('fs');
+const Shoppy = require('Shoppy.gg');
+const API = new Shoppy.API(config.apiKey);
 bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
